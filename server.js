7585c0da83fe7feda0445d3c7ed810108dac78e5
Aryan Pundir <95073218+Pundiraryan@users.noreply.github.com>
2024-01-08 07:58:09 +0530

    backend server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { connectToMongoDB } = require("./config");
 const { userRoutes, chatRoutes, messageRoutes } = require("./routes");
 const { notFound, errorHandler } = require("./middleware");
 
-const app = express(); // Use express js in our app
+const app = express(); 
 app.use(express.json()); // Accept JSON data
 dotenv.config({ path: path.join(__dirname, "./.env") }); // Specify a custom path if your file containing environment variables is located elsewhere
 connectToMongoDB(); // Connect to Database
